199ad401cc568f45ab7bdb9cc6c285a59264ac0d
Yerrumsetty Bharath <yerrumsett.bharath@gmail.com>
2021-09-24 23:24:49 +0530

    delete function added

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ class Calc {
     constructor(historyText, currentText) {
         this.historyText = historyText;
         this.currentText = currentText;
-
+        this.previousString = '';
     }
 
     clear() {
@@ -11,7 +11,8 @@ class Calc {
         this.operation = undefined;
     }
 
-    delet() {
+    delete() {
+        this.currentString = this.currentString.toString().slice(0,-1);
 
     }
 
@@ -20,26 +21,73 @@ class Calc {
             this.currentString = number;
         }
         else {
-            this.currentString = this.currentString.toString() + number.toString();
+
+            if (number === "." && this.currentString.includes(".")) {
+                return;
+            }
+            else {
+
+                this.currentString = this.currentString.toString() + number.toString();
+            }
         }
 
     }
 
     updateDisplay() {
         this.currentText.innerText = this.currentString;
+        this.historyText.innerText = this.previousString;
 
     }
 
     chooseOperation(operation) {
+        if (this.currentString === "") return;
 
+        //auto compute whenever previous string is not null using the operation given
+        if (this.previousString !== "") {
+            this.compute();
+        }
+        this.operation = operation;
+        console.log(this.operation);
+        this.previousString = this.currentString + operation;
+        this.currentString = "";
     }
 
     compute() {
+        let res;
+        const prev = parseFloat(this.previousString);
+        const curr = parseFloat(this.currentString);
 
+        //if an operation is given instead of a number, then just ignore it 
+        if (isNaN(prev) || isNaN(curr)) return;
+
+        switch (this.operation) {
+            case "+":
+                res = prev + curr;
+                break;
+
+
+            case "-":
+                res = prev - curr;
+                break;
+
+            case "/":
+                res = prev / curr;
+                break;
+
+            case "*":
+                res = prev * curr;
+                break;
+
+            default:
+                return;
+        }
+
+        this.currentString = res;
+        this.operation = undefined;
+        this.previousString = "";
     }
-
-
 }
+
 
 
 
@@ -61,4 +109,27 @@ numberButtons.forEach(item => {
         calc.appendNumber(item.innerText);
         calc.updateDisplay();
     })
+})
+
+operatorButtons.forEach((item) => {
+    item.addEventListener("click", () => {
+        calc.chooseOperation(item.innerText);
+        calc.updateDisplay();
+    })
+})
+
+
+equalsButton.addEventListener('click', () => {
+    calc.compute();
+    calc.updateDisplay();
+})
+
+allClear.addEventListener('click', () => {
+    calc.clear();
+    calc.updateDisplay();
+})
+
+deleteButton.addEventListener('click', () => {
+    calc.delete();
+    calc.updateDisplay();
 })
